a7130531bb2924cb3c227bde266ada307aa38f83
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-06 13:30:12 +0200

    Adding profile image pt.1 + now Each user in the db is considred a Customer

--- fastparcel/settings.py
+++ fastparcel/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.ProfileMiddleware'
 ]
 
 ROOT_URLCONF = 'fastparcel.urls'
