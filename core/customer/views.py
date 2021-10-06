273c02340e25884ef2b9c6c9aa8e6f05ce8ace5c
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-06 14:04:08 +0200

    Toast message to show updates on profile page

--- core/customer/views.py
+++ core/customer/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from core.customer import forms
 
-
+from django.contrib import messages
 
 @login_required()
 def home(request):
@@ -22,6 +22,8 @@ def profile_page(request):
         if user_form.is_valid() and customer_form.is_valid():
             user_form.save()
             customer_form.save()
+
+            messages.success(request,'Your Profile has been updated')
             return redirect(reverse('customer:profile'))
 
     return render(request, 'customer/profile.html',{
