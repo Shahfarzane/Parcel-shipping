bea7f1f5d7aae686ff27630fb1af32e64c7e626c
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-08 11:38:25 +0200

    Showing stripe card / removing the card from stripe **NEEDS SOME FIXINGS

--- core/customer/views.py
+++ core/customer/views.py
@@ -66,6 +66,13 @@ def profile_page(request):
 def payment_method_page(request):
     current_customer = request.user.customer
 
+    # remove existing card information
+    if request.method == 'POST':
+        stripe.PaymentMethod.detach(current_customer.stripe_payment_method_id)
+        current_customer.stripe_payment_method_id = ""
+        current_customer.stripe_card_last4 = ""
+        current_customer.save()
+        return redirect(reverse('customer:payment_method'))
     #save stripe customer information
     if not current_customer.stripe_customer_id:
         customer = stripe.Customer.create()
@@ -90,13 +97,18 @@ def payment_method_page(request):
         current_customer.stripe_card_last4 = ""
         current_customer.save()
 
-    intent = stripe.SetupIntent.create(
-        customer = current_customer.stripe_customer_id
-    )
-    return render(request, 'customer/payment_method.html',{
-        "client_secret": intent.client_secret,
-        "STRIPE_API_PUBLIC_KEY": settings.STRIPE_API_PUBLIC_KEY,
-    })
+
+    if not current_customer.stripe_payment_method_id:
+        intent = stripe.SetupIntent.create(
+            customer = current_customer.stripe_customer_id
+        )
+        return render(request, 'customer/payment_method.html',{
+            "client_secret": intent.client_secret,
+            "STRIPE_API_PUBLIC_KEY": settings.STRIPE_API_PUBLIC_KEY,
+        })
+    else:
+
+        return render(request, 'customer/payment_method.html')   
 
 
 
