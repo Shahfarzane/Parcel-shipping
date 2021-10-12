a6db078401380ec8aa30b541a1ff4ea187180794
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-12 09:42:16 +0200

    The job creating form - Step 2 )

--- core/customer/views.py
+++ core/customer/views.py
@@ -123,6 +123,7 @@ def create_job_page(request):
 
     creating_job = Job.objects.filter(customer=current_customer, status=Job.CREATING_STATUS).last()
     step1_form = forms.JobCreateStep1Form(instance=creating_job)
+    step2_form = forms.JobCreateStep2Form(instance=creating_job)
 
     if request.method == "POST":
         if request.POST.get('step') == '1':
@@ -141,9 +142,10 @@ def create_job_page(request):
         current_step = 2
 
     return render(request, 'customer/create_job.html',{
-        "step1_form": step1_form,
         "job": creating_job,
         "step": current_step,
+        "step2_form": step2_form,
+        "step1_form": step1_form,
         
     })
 
