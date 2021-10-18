f18407907a8feffe9d16cae6bcae28755cc0a7bb
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-18 10:16:08 +0200

    Showing google maps for available jobs page

--- core/courier/views.py
+++ core/courier/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.conf import settings
 
 
 
@@ -8,10 +9,8 @@ from django.urls import reverse
 def home(request):
     return redirect(reverse('courier:available_jobs'))
 
-
-
-
-
 @login_required(login_url="/sign-in/?next=/courier/")
 def available_jobs_page(request):
-    return render(request,'courier/available_jobs.html')
+    return render(request, 'courier/available_jobs.html', {
+        "GOOGLE_MAP_API_KEY": settings.GOOGLE_MAP_API_KEY
+    })
