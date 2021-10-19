b0af62b89c3cf26b9608c4155e092c43193851e7
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-19 10:29:19 +0200

    Showing the Job/task details by clicking on the job

--- core/courier/views.py
+++ core/courier/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.conf import settings
-
+from core.models import *
 
 
 @login_required(login_url="/sign-in/?next=/courier/")
@@ -13,4 +13,15 @@ def home(request):
 def available_jobs_page(request):
     return render(request, 'courier/available_jobs.html', {
         "GOOGLE_MAP_API_KEY": settings.GOOGLE_MAP_API_KEY
+    })
+
+
+@login_required(login_url="/sign-in/?next=/courier/")
+def available_job_page(request,id):
+    job = Job.objects.filter(id=id, status=Job.PROCESSING_STATUS).last()
+
+    if not job:
+        return redirect(reverse('courier:available_jobs'))
+    return render(request, 'courier/available_job.html',{
+        "job": job,
     })
