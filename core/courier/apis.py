25ca8ae4f53175e0a191897488c992650b03f378
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-21 09:28:34 +0200

    Uploading the photo to the database

--- core/courier/apis.py
+++ core/courier/apis.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from core.models import *
+from django.utils import timezone
 
 @csrf_exempt
 @login_required(login_url="/courier/sign-in/")
@@ -13,4 +14,27 @@ def available_jobs_api(request):
     "jobs": jobs
   })
 
+
+
+@csrf_exempt
+@login_required(login_url="/courier/sign-in/")
+def current_job_update_api(request,id):
+      job = Job.objects.filter(
+        id=id,
+        courier = request.user.courier,
+        status__in=[
+          Job.PICKING_STATUS,
+          Job.DELIVERING_STATUS
+        ]
+      ).last()
+
+      if job.status == Job.PICKING_STATUS:
+            job.pickup_photo = request.FILES['pickup_photo']
+            job.pickedup_at = timezone.now()
+            job.status = Job.DELIVERING_STATUS
+            job.save()
+
+      return JsonResponse({
+        "success": True,
+      })      
 
