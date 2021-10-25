14459acd730d89c4e5c0f3ccc5ac93e653af617f
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-25 08:12:31 +0200

    Updates to Transaction model . PAYOUT pt.1

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from . import models
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['stripe_payment_intent_id', 'customer', 'courier','job', 'amount', 'status','create_at','courier_paypal_email' ]
+    list_filter = ['status',]
+
+    def customer(self,obj):
+        return obj.job.customer
+
+    def courier(self, obj):
+        return obj.job.courier
+    def courier_paypal_email(sefl, obj):
+        return obj.job.courier.paypal_email if obj.job.courier else None        
+
+
 # Register your models here.
 
 admin.site.register(models.Customer)
 admin.site.register(models.Courier)
 admin.site.register(models.Category)
 admin.site.register(models.Job)
-admin.site.register(models.Transaction)
+admin.site.register(models.Transaction,TransactionAdmin)
