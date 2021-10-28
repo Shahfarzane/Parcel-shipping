68d86c23fc38a87e3b8c2e72d882b31f97030d1d
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-10-28 09:35:51 +0200

    redis 2

--- fastparcel/asgi.py
+++ fastparcel/asgi.py
@@ -1,11 +1,12 @@
 import os
+from django.core.asgi import get_asgi_application
+
 # Fetch Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers and AuthMiddlewareStack that may import ORM
 # models.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastparcel.settings")
 django_asgi_app = get_asgi_application()
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from fastparcel.urls import websocket_urlpatterns
 
